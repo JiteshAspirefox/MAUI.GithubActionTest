99b44165380f559a5514d711f6d5d22b2d842a7a
Jitesh Kumar <anjit193023@gmail.com>
2024-02-06 00:04:37 +0530

    Adding basic project files

---
+++ FileBringer.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FileBringer.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
